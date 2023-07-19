3570eaef0bfd537eed706ebc15945aee0a6be479
Anikribhu <95336760+Anikribhu@users.noreply.github.com>
2023-07-19 13:39:53 +0530

    Contact added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,7 +10,7 @@ const Home = () => {
 
     const[letterClass,setletterClass] = useState('text-animate')
     const nameArray = ['r','e','s','t','h','a']
-    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r']
+    const jobArray = ['W','e','b',' ','d','e','v','e','l','o','p','e','r']
 
     useEffect(() => {
         setTimeout(() => {
